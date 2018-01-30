493e35d20b7026d813e57f1aee45085512778d5e
Unknown <romteule@gmail.com>
2018-01-30 16:03:20 +0100

    feat(Excel) : Added skeleton for Excel in all stages of file processing, for further references, please refer to Area B2 of documentation

---
+++ GENOME/src/Excel/ExcelWriter.java
@@ -0,0 +1,44 @@
+package Excel;
+
+import Data.Organism;
+import Data.Replicon;
+import Data.Kingdom;
+import Data.SubGroup;
+import Data.Group;
+
+public class ExcelWriter {
+	
+	
+	/**
+	 * Function call to write kingdom summary in Excel workbook
+	 */
+	public void writeKingdom(Kingdom _kingdom)
+	{
+		return;
+	}
+	
+	/**
+	 * Function call to write group summary in Excel workbook
+	 */
+	public void writeGroup(Group _group)
+	{
+		return;
+	}
+	
+	/**
+	 * Function call to write subgroup summary in Excel workbook
+	 */
+	public void writeSubGroup(SubGroup _subGroup	)
+	{
+		return;
+	}
+	
+	/**
+	 * Function call to write organism summary in Excel workbook
+	 */
+	public void writeOrganism(Organism _organism)
+	{
+		return;
+	}
+
+}
