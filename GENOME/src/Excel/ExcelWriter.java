f3cfc40b5263f38b64f2e78da5b4dcb445f78c5f
Unknown <romteule@gmail.com>
2018-02-20 22:13:47 +0100

    feat(Excel) : added logic for organism in Writer, added FileManager

--- GENOME/src/Excel/ExcelWriter.java
+++ GENOME/src/Excel/ExcelWriter.java
@@ -1,5 +1,6 @@
 package Excel;
 
+import Data.Kingdom;
 import Data.Organism;
 import Data.Replicon;
 /*import Data.Kingdom;
@@ -16,15 +17,15 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelWriter {
-	
-	
+
+
 	public static void Test() throws InvalidFormatException, IOException {
 		    // Obtain a workbook from the excel file
 		    Workbook workbook = WorkbookFactory.create(new File("templateExemple.xlsx"));
 
 		    // Get Sheet at index 0
 		    Sheet sheet = workbook.getSheetAt(0);
-		    
+
 		    if (sheet == null)
 		    {
 		    	sheet= workbook.createSheet();
@@ -36,7 +37,7 @@ public class ExcelWriter {
 		    {
 		    	row=sheet.createRow(1);
 		    }
-		    
+
 		    // Get the Cell at index 2 from the above row
 		    Cell cell = row.getCell(2);
 
@@ -47,25 +48,39 @@ public class ExcelWriter {
 		    // Update the cell's value
 		    cell.setCellType(CellType.STRING);
 		    cell.setCellValue("Updated Value");
-		    
+
 		    // Write the output to the file
 		    FileOutputStream fileOut = new FileOutputStream("resultatExemple.xlsx");
 		    workbook.write(fileOut);
 		    fileOut.close();
-		
+
 
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Function call to write kingdom summary in Excel workbook
 	 */
-	public void writeKingdom(/*Kingdom _kingdom*/)
+	public void writeKingdom(Kingdom _kingdom)
 	{
+	    //get path from file manager
+        String Path = FileManager.GetPathFor(_kingdom);
+        //check if file exist
+
+        //yes, delete it? Keep general info?
+
+        //create proper file
+
+        //LE fun qui commence
+
+        //create workbook
+
+        //create sheet summary
+
 		return;
 	}
-	
+
 	/**
 	 * Function call to write group summary in Excel workbook
 	 */
@@ -73,7 +88,7 @@ public class ExcelWriter {
 	{
 		return;
 	}
-	
+
 	/**
 	 * Function call to write subgroup summary in Excel workbook
 	 */
@@ -81,14 +96,51 @@ public class ExcelWriter {
 	{
 		return;
 	}
-	
+
 	/**
 	 * Function call to write organism summary in Excel workbook
 	 */
 	public void writeOrganism(Organism _organism)
 	{
-		return;
+		//get path from file manager
+        String Path = FileManager.GetPathFor(_organism);
+        //check if file exist
+
+        //yes, delete it? Keep general info?
+
+        //LE fun qui commence
+
+        //create workbook
+
+        //create general info
+
+
+        //Summary sheets
+
+            //for each Summary page, create sheet and fill it
+
+        //for each replicon
+
+                //create sheet
+
+                //create header
+
+                //for each trinucleotide
+
+                    //fill statistics ( cf StatisticsTest)
+
+                //for each dinucleotid
+
+                    //fill statistics ( cf StatisticsTest)
+
+        //save with correct name
+
+
+        //Success?
+
+
+        return;
 	}
-	
-	
+
+
 }
--- GENOME/src/Excel/FileManager.java
+++ GENOME/src/Excel/FileManager.java
@@ -1,5 +1,57 @@
 package Excel;
 
-public class FileManager {
+import Data.Kingdom;
+import Data.Organism;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class FileManager {
+
+    private static String s_RootFolderPath;
+    private static String s_RootFolderName="Results";
+
+    private  FileManager(){
+        s_RootFolderPath=System.getProperty("user.dir")+s_RootFolderName;
+    }
+
+
+
+    /*
+    * Get the path for the kingdom folder, create it if need
+     */
+    public static String GetPathFor(Kingdom _kindgom){
+
+
+        String KingdomPath= s_RootFolderPath+"/"+_kindgom.getName();
+        Path Path= Paths.get(KingdomPath);
+
+        if(Files.notExists(Path))
+        {
+
+        }
+
+        return  "oui";
+    }
+
+    private  void CreatePathFor(Organism _organism)
+    {
+
+    }
+
+    public static String GetPathFor(Organism _organism){
+        /*String Root= System.getProperty("user.dir")+s_RootFolder;
+
+        String Path=Root;
+
+
+
+
+
+
+        return  Path+"/"+_organism.getName()+".xlsx";*/
+        return  "oui";
+    }
 }
