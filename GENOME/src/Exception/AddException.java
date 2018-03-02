57aaea9b920fe925be82d7986f879044bdff7930
pamplemousse26 <romain.manciaux@gmail.com>
2018-03-02 23:12:20 +0100

    feat(Exception): add custom Exception

---
+++ GENOME/src/Exception/AddException.java
@@ -0,0 +1,9 @@
+package Exception;
+
+public final class AddException extends IException {
+
+    public AddException(String _message){
+        super(_message);
+    }
+
+}
---
+++ GENOME/src/Exception/MissException.java
@@ -0,0 +1,9 @@
+package Exception;
+
+public final class MissException extends IException{
+
+    public MissException(String _message){
+        super(_message);
+    }
+
+}
